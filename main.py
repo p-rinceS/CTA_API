ee669a4de6bbeaac802efa65c36a5c8a31ebbccd
Prince Sonani <princesonani9112@gmail.com>
2023-01-04 01:03:08 -0600

    comments :)

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+#pkg imports:
 from flask import *
 import json, time
 app = Flask(__name__)
 
 
 @app.route('/', methods = ['GET'])
-
+#creating ROUTE..
+# Here we are creating home page (This is the default page that is loaded once api is opened.)
 def home_page():
     data_set = {"Page": "Home", "Message": "Successfully Loaded", "Timestamp": time.time()}
     json_dump = json.dumps(data_set)
@@ -13,9 +15,13 @@ def home_page():
     return json_dump
 
 @app.route('/user/', methods = ['GET'])
-
+#creating ROUTE..
+# Here we create another route or path.. this time to a request page. Just so we know it works...
+# We created a user query to identify that it changes based on the url change.
+# Hooray!
 def request_page():
     user_query = str(request.args.get('user')) # .../user/?user=USER_NAME
+
 
     data_set = {"Page": "Request", "Message": f"Successfully Loaded Request Page for {user_query}", "Timestamp": time.time()}
     json_dump = json.dumps(data_set)
@@ -23,5 +29,6 @@ def request_page():
     
     return json_dump
 
+#Let's simply run the whole program below..
 if __name__ == "__main__":
     app.run(port=7777)
